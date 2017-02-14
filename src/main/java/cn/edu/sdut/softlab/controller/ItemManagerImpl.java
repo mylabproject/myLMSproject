5310263bd6b1d897e15f82c0f8113f76343537db
GaoYisheng <1160139211@qq.com>
2017-02-14 21:28:37 +0800

    =>

--- src/main/java/cn/edu/sdut/softlab/controller/ItemManagerImpl.java
+++ src/main/java/cn/edu/sdut/softlab/controller/ItemManagerImpl.java
@@ -14,10 +14,10 @@ import javax.inject.Named;
 
 import javax.transaction.UserTransaction;
 
-
 /**
- *   物品管理器.
- *  @author gaoyisheng
+ * 物品管理器.
+ * 
+ * @author gaoyisheng
  */
 @Named("itemManager")
 @RequestScoped
@@ -32,13 +32,13 @@ public class ItemManagerImpl implements ItemManager {
 	private UserTransaction utx;
 
 	private Item newItem = new Item();
-							    //不能在此处 直接new()是因为，
-							   //boughtTime 这个属性 不是在 页面初始化 的时候，初始化。而是
-						        //应该放在Add 这个button 的单击事件里。
-	
-								//上面分析有误， 。。。。。so ?
+	// 不能在此处 直接new()是因为，
+	// boughtTime 这个属性 不是在 页面初始化 的时候，初始化。而是
+	// 应该放在Add 这个button 的单击事件里。
 
-	//Item 的 set/get 方法
+	// 上面分析有误， 。。。。。so ?
+
+	// Item 的 set/get 方法
 	public Item getNewItem() {
 		return newItem;
 	}
@@ -64,13 +64,30 @@ public class ItemManagerImpl implements ItemManager {
 	public String addItem() throws Exception {
 		try {
 			utx.begin();
-			/*newItem.setDateBought(new Date());*/
+			/* newItem.setDateBought(new Date()); */
 			itemService.create(newItem);
 			logger.log(Level.INFO, "Added {0}", newItem);
 			return "/AdministratorHome_additems.xhtml?faces-redirect=true";
 		} finally {
 			utx.commit();
 		}
+	}
+
+	/**
+	 * 1.从前台获取 name 
+	 * 2.到数据库中查询 
+	 * 3.修改其中 totol字段（入库+=n 出库-=n， 后期交与不同的sevice负责，松耦合提取出来 ） 
+	 * 4.保存记录到itme_account表，（创建一个新的字段，为以后查出入库明细 用）
+	 *
+	 * @return
+	 * @throws Exception
+	 */
+	@RequestScoped
+	public String itemOut() throws Exception {
+		utx.begin();
+		//
+
+		return " ";
 	}
 
 }
