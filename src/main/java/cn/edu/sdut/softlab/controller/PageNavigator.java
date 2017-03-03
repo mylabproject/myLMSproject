57cb4d9a20ad0752377d5ccb7c421a1ac3c16147
GaoYisheng <1160139211@qq.com>
2017-03-03 21:32:42 +0800

    structure of directory & page

--- src/main/java/cn/edu/sdut/softlab/controller/PageNavigator.java
+++ src/main/java/cn/edu/sdut/softlab/controller/PageNavigator.java
@@ -7,6 +7,10 @@ package cn.edu.sdut.softlab.controller;
  * @author gaoyisheng
  *
  */
+
+//@     View(name="error")
+//To deal with deltaspike
+
 public class PageNavigator {
 	
 	public String goLogin(){
